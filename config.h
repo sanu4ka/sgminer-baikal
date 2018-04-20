b7eee7eeb46a4dc55ab4182f2cfb94654372cc5c
sanu4ka <37982710+sanu4ka@users.noreply.github.com>
2018-04-20 21:52:37 +0300

    Add files via upload

--- config.h
+++ config.h
@@ -2,324 +2,324 @@
 /* config.h.in.  Generated from configure.ac by autoheader.  */
 
 /* Define to the number of bits in type 'ptrdiff_t'. */
-#undef BITSIZEOF_PTRDIFF_T
+/* #undef BITSIZEOF_PTRDIFF_T */
 
 /* Define to the number of bits in type 'sig_atomic_t'. */
-#undef BITSIZEOF_SIG_ATOMIC_T
+/* #undef BITSIZEOF_SIG_ATOMIC_T */
 
 /* Define to the number of bits in type 'size_t'. */
-#undef BITSIZEOF_SIZE_T
+/* #undef BITSIZEOF_SIZE_T */
 
 /* Define to the number of bits in type 'wchar_t'. */
-#undef BITSIZEOF_WCHAR_T
+/* #undef BITSIZEOF_WCHAR_T */
 
 /* Define to the number of bits in type 'wint_t'. */
-#undef BITSIZEOF_WINT_T
+/* #undef BITSIZEOF_WINT_T */
 
 /* Define to one of `_getb67', `GETB67', `getb67' for Cray-2 and Cray-YMP
    systems. This function is required for `alloca.c' support on those systems.
    */
-#undef CRAY_STACKSEG_END
+/* #undef CRAY_STACKSEG_END */
 
 /* Defined if version of curl supports keepalive. */
-#undef CURL_HAS_KEEPALIVE
+#define CURL_HAS_KEEPALIVE 1
 
 /* Define to 1 if using `alloca.c'. */
-#undef C_ALLOCA
+/* #undef C_ALLOCA */
 
 /* int execv(const char*, const char*const*); */
-#undef EXECV_2ND_ARG_TYPE
+#define EXECV_2ND_ARG_TYPE char* const*
 
 /* Define to 1 when the gnulib module memchr should be tested. */
-#undef GNULIB_TEST_MEMCHR
+#define GNULIB_TEST_MEMCHR 1
 
 /* Define to 1 when the gnulib module memmem should be tested. */
-#undef GNULIB_TEST_MEMMEM
+#define GNULIB_TEST_MEMMEM 1
 
 /* Define to 1 when the gnulib module sigaction should be tested. */
-#undef GNULIB_TEST_SIGACTION
+#define GNULIB_TEST_SIGACTION 1
 
 /* Define to 1 when the gnulib module sigprocmask should be tested. */
-#undef GNULIB_TEST_SIGPROCMASK
+#define GNULIB_TEST_SIGPROCMASK 1
 
 /* Defined if ADL headers were found */
-#undef HAVE_ADL
+/* #undef HAVE_ADL */
 
 /* Define to 1 if you have `alloca', as a function or macro. */
-#undef HAVE_ALLOCA
+#define HAVE_ALLOCA 1
 
 /* Define to 1 if you have <alloca.h> and it should be used (not on Ultrix).
    */
-#undef HAVE_ALLOCA_H
+#define HAVE_ALLOCA_H 1
 
 /* Define if __attribute__((cold)) */
-#undef HAVE_ATTRIBUTE_COLD
+#define HAVE_ATTRIBUTE_COLD 1
 
 /* Define if __attribute__((const)) */
-#undef HAVE_ATTRIBUTE_CONST
+#define HAVE_ATTRIBUTE_CONST 1
 
 /* Define if __attribute__((noreturn)) */
-#undef HAVE_ATTRIBUTE_NORETURN
+#define HAVE_ATTRIBUTE_NORETURN 1
 
 /* Define if __attribute__((format(__printf__))) */
-#undef HAVE_ATTRIBUTE_PRINTF
+#define HAVE_ATTRIBUTE_PRINTF 1
 
 /* Define if __attribute__((unused)) */
-#undef HAVE_ATTRIBUTE_UNUSED
+#define HAVE_ATTRIBUTE_UNUSED 1
 
 /* Define if __attribute__((used)) */
-#undef HAVE_ATTRIBUTE_USED
+#define HAVE_ATTRIBUTE_USED 1
 
 /* Define to 1 if you have the <bp-sym.h> header file. */
-#undef HAVE_BP_SYM_H
+/* #undef HAVE_BP_SYM_H */
 
 /* Define if have __builtin_constant_p */
-#undef HAVE_BUILTIN_CONSTANT_P
+#define HAVE_BUILTIN_CONSTANT_P 1
 
 /* Define if have __builtin_types_compatible_p */
-#undef HAVE_BUILTIN_TYPES_COMPATIBLE_P
+#define HAVE_BUILTIN_TYPES_COMPATIBLE_P 1
 
 /* Defined to 1 if curses TUI support is wanted */
-#undef HAVE_CURSES
+#define HAVE_CURSES 1
 
 /* Define to 1 if you have the declaration of `memmem', and to 0 if you don't.
    */
-#undef HAVE_DECL_MEMMEM
+#define HAVE_DECL_MEMMEM 1
 
 /* Define to 1 if you have the <dlfcn.h> header file. */
-#undef HAVE_DLFCN_H
+#define HAVE_DLFCN_H 1
 
 /* Define to 1 if you have the <inttypes.h> header file. */
-#undef HAVE_INTTYPES_H
+#define HAVE_INTTYPES_H 1
 
 /* Defined to 1 if libcurl support built in */
-#undef HAVE_LIBCURL
+#define HAVE_LIBCURL 1
 
 /* Define if you have libusb-1.0 */
-#undef HAVE_LIBUSB
+/* #undef HAVE_LIBUSB */
 
 /* Define to 1 if you have the `winpthread' library (-lwinpthread). */
-#undef HAVE_LIBWINPTHREAD
+/* #undef HAVE_LIBWINPTHREAD */
 
 /* Define to 1 if the system has the type `long long int'. */
-#undef HAVE_LONG_LONG_INT
+#define HAVE_LONG_LONG_INT 1
 
 /* Define to 1 if mmap()'s MAP_ANONYMOUS flag is available after including
    config.h and <sys/mman.h>. */
-#undef HAVE_MAP_ANONYMOUS
+#define HAVE_MAP_ANONYMOUS 1
 
 /* Define to 1 if you have the `memmem' function. */
-#undef HAVE_MEMMEM
+#define HAVE_MEMMEM 1
 
 /* Define to 1 if you have the <memory.h> header file. */
-#undef HAVE_MEMORY_H
+#define HAVE_MEMORY_H 1
 
 /* Define to 1 if you have the `mprotect' function. */
-#undef HAVE_MPROTECT
+#define HAVE_MPROTECT 1
 
 /* Define to 1 if memmem is declared even after undefining macros. */
-#undef HAVE_RAW_DECL_MEMMEM
+#define HAVE_RAW_DECL_MEMMEM 1
 
 /* Define to 1 if mempcpy is declared even after undefining macros. */
-#undef HAVE_RAW_DECL_MEMPCPY
+#define HAVE_RAW_DECL_MEMPCPY 1
 
 /* Define to 1 if memrchr is declared even after undefining macros. */
-#undef HAVE_RAW_DECL_MEMRCHR
+#define HAVE_RAW_DECL_MEMRCHR 1
 
 /* Define to 1 if rawmemchr is declared even after undefining macros. */
-#undef HAVE_RAW_DECL_RAWMEMCHR
+#define HAVE_RAW_DECL_RAWMEMCHR 1
 
 /* Define to 1 if sigaction is declared even after undefining macros. */
-#undef HAVE_RAW_DECL_SIGACTION
+#define HAVE_RAW_DECL_SIGACTION 1
 
 /* Define to 1 if sigaddset is declared even after undefining macros. */
-#undef HAVE_RAW_DECL_SIGADDSET
+#define HAVE_RAW_DECL_SIGADDSET 1
 
 /* Define to 1 if sigdelset is declared even after undefining macros. */
-#undef HAVE_RAW_DECL_SIGDELSET
+#define HAVE_RAW_DECL_SIGDELSET 1
 
 /* Define to 1 if sigemptyset is declared even after undefining macros. */
-#undef HAVE_RAW_DECL_SIGEMPTYSET
+#define HAVE_RAW_DECL_SIGEMPTYSET 1
 
 /* Define to 1 if sigfillset is declared even after undefining macros. */
-#undef HAVE_RAW_DECL_SIGFILLSET
+#define HAVE_RAW_DECL_SIGFILLSET 1
 
 /* Define to 1 if sigismember is declared even after undefining macros. */
-#undef HAVE_RAW_DECL_SIGISMEMBER
+#define HAVE_RAW_DECL_SIGISMEMBER 1
 
 /* Define to 1 if sigpending is declared even after undefining macros. */
-#undef HAVE_RAW_DECL_SIGPENDING
+#define HAVE_RAW_DECL_SIGPENDING 1
 
 /* Define to 1 if sigprocmask is declared even after undefining macros. */
-#undef HAVE_RAW_DECL_SIGPROCMASK
+#define HAVE_RAW_DECL_SIGPROCMASK 1
 
 /* Define to 1 if stpcpy is declared even after undefining macros. */
-#undef HAVE_RAW_DECL_STPCPY
+#define HAVE_RAW_DECL_STPCPY 1
 
 /* Define to 1 if stpncpy is declared even after undefining macros. */
-#undef HAVE_RAW_DECL_STPNCPY
+#define HAVE_RAW_DECL_STPNCPY 1
 
 /* Define to 1 if strcasestr is declared even after undefining macros. */
-#undef HAVE_RAW_DECL_STRCASESTR
+#define HAVE_RAW_DECL_STRCASESTR 1
 
 /* Define to 1 if strchrnul is declared even after undefining macros. */
-#undef HAVE_RAW_DECL_STRCHRNUL
+#define HAVE_RAW_DECL_STRCHRNUL 1
 
 /* Define to 1 if strdup is declared even after undefining macros. */
-#undef HAVE_RAW_DECL_STRDUP
+#define HAVE_RAW_DECL_STRDUP 1
 
 /* Define to 1 if strerror_r is declared even after undefining macros. */
-#undef HAVE_RAW_DECL_STRERROR_R
+#define HAVE_RAW_DECL_STRERROR_R 1
 
 /* Define to 1 if strncat is declared even after undefining macros. */
-#undef HAVE_RAW_DECL_STRNCAT
+#define HAVE_RAW_DECL_STRNCAT 1
 
 /* Define to 1 if strndup is declared even after undefining macros. */
-#undef HAVE_RAW_DECL_STRNDUP
+#define HAVE_RAW_DECL_STRNDUP 1
 
 /* Define to 1 if strnlen is declared even after undefining macros. */
-#undef HAVE_RAW_DECL_STRNLEN
+#define HAVE_RAW_DECL_STRNLEN 1
 
 /* Define to 1 if strpbrk is declared even after undefining macros. */
-#undef HAVE_RAW_DECL_STRPBRK
+#define HAVE_RAW_DECL_STRPBRK 1
 
 /* Define to 1 if strsep is declared even after undefining macros. */
-#undef HAVE_RAW_DECL_STRSEP
+#define HAVE_RAW_DECL_STRSEP 1
 
 /* Define to 1 if strsignal is declared even after undefining macros. */
-#undef HAVE_RAW_DECL_STRSIGNAL
+#define HAVE_RAW_DECL_STRSIGNAL 1
 
 /* Define to 1 if strtok_r is declared even after undefining macros. */
-#undef HAVE_RAW_DECL_STRTOK_R
+#define HAVE_RAW_DECL_STRTOK_R 1
 
 /* Define to 1 if strverscmp is declared even after undefining macros. */
-#undef HAVE_RAW_DECL_STRVERSCMP
+#define HAVE_RAW_DECL_STRVERSCMP 1
 
 /* Define to 1 if you have the `sigaction' function. */
-#undef HAVE_SIGACTION
+#define HAVE_SIGACTION 1
 
 /* Define to 1 if you have the `sigaltstack' function. */
-#undef HAVE_SIGALTSTACK
+#define HAVE_SIGALTSTACK 1
 
 /* Define to 1 if the system has the type `siginfo_t'. */
-#undef HAVE_SIGINFO_T
+/* #undef HAVE_SIGINFO_T */
 
 /* Define to 1 if you have the `siginterrupt' function. */
-#undef HAVE_SIGINTERRUPT
+#define HAVE_SIGINTERRUPT 1
 
 /* Define to 1 if 'sig_atomic_t' is a signed integer type. */
-#undef HAVE_SIGNED_SIG_ATOMIC_T
+/* #undef HAVE_SIGNED_SIG_ATOMIC_T */
 
 /* Define to 1 if 'wchar_t' is a signed integer type. */
-#undef HAVE_SIGNED_WCHAR_T
+/* #undef HAVE_SIGNED_WCHAR_T */
 
 /* Define to 1 if 'wint_t' is a signed integer type. */
-#undef HAVE_SIGNED_WINT_T
+/* #undef HAVE_SIGNED_WINT_T */
 
 /* Define to 1 if the system has the type `sigset_t'. */
-#undef HAVE_SIGSET_T
+/* #undef HAVE_SIGSET_T */
 
 /* Define to 1 if you have the <stdint.h> header file. */
-#undef HAVE_STDINT_H
+#define HAVE_STDINT_H 1
 
 /* Define to 1 if you have the <stdlib.h> header file. */
-#undef HAVE_STDLIB_H
+#define HAVE_STDLIB_H 1
 
 /* Define to 1 if you have the <strings.h> header file. */
-#undef HAVE_STRINGS_H
+#define HAVE_STRINGS_H 1
 
 /* Define to 1 if you have the <string.h> header file. */
-#undef HAVE_STRING_H
+#define HAVE_STRING_H 1
 
 /* Define to 1 if `sa_sigaction' is a member of `struct sigaction'. */
-#undef HAVE_STRUCT_SIGACTION_SA_SIGACTION
+#define HAVE_STRUCT_SIGACTION_SA_SIGACTION 1
 
 /* Define to 1 if you have the <syslog.h> header file. */
-#undef HAVE_SYSLOG_H
+#define HAVE_SYSLOG_H 1
 
 /* Define to 1 if you have the <sys/bitypes.h> header file. */
-#undef HAVE_SYS_BITYPES_H
+/* #undef HAVE_SYS_BITYPES_H */
 
 /* Define to 1 if you have the <sys/inttypes.h> header file. */
-#undef HAVE_SYS_INTTYPES_H
+/* #undef HAVE_SYS_INTTYPES_H */
 
 /* Define to 1 if you have the <sys/mman.h> header file. */
-#undef HAVE_SYS_MMAN_H
+#define HAVE_SYS_MMAN_H 1
 
 /* Define to 1 if you have the <sys/stat.h> header file. */
-#undef HAVE_SYS_STAT_H
+#define HAVE_SYS_STAT_H 1
 
 /* Define to 1 if you have the <sys/types.h> header file. */
-#undef HAVE_SYS_TYPES_H
+#define HAVE_SYS_TYPES_H 1
 
 /* Define to 1 if you have the <unistd.h> header file. */
-#undef HAVE_UNISTD_H
+#define HAVE_UNISTD_H 1
 
 /* Define to 1 if the system has the type `unsigned long long int'. */
-#undef HAVE_UNSIGNED_LONG_LONG_INT
+#define HAVE_UNSIGNED_LONG_LONG_INT 1
 
 /* Define if __attribute__((warn_unused_result)) */
-#undef HAVE_WARN_UNUSED_RESULT
+#define HAVE_WARN_UNUSED_RESULT 1
 
 /* Define to 1 if you have the <wchar.h> header file. */
-#undef HAVE_WCHAR_H
+#define HAVE_WCHAR_H 1
 
 /* Define if you have the 'wchar_t' type. */
-#undef HAVE_WCHAR_T
+#define HAVE_WCHAR_T 1
 
 /* Define to the sub-directory where libtool stores uninstalled libraries. */
-#undef LT_OBJDIR
+#define LT_OBJDIR ".libs/"
 
 /* Define to a substitute value for mmap()'s MAP_ANONYMOUS flag. */
-#undef MAP_ANONYMOUS
+/* #undef MAP_ANONYMOUS */
 
 /* Name of package */
-#undef PACKAGE
+#define PACKAGE "sgminer"
 
 /* Define to the address where bug reports for this package should be sent. */
-#undef PACKAGE_BUGREPORT
+#define PACKAGE_BUGREPORT "veox@wemakethings.net"
 
 /* Define to the full name of this package. */
-#undef PACKAGE_NAME
+#define PACKAGE_NAME "sgminer"
 
 /* Define to the full name and version of this package. */
-#undef PACKAGE_STRING
+#define PACKAGE_STRING "sgminer 5.5.0-b"
 
 /* Define to the one symbol short name of this package. */
-#undef PACKAGE_TARNAME
+#define PACKAGE_TARNAME "sgminer"
 
 /* Define to the home page for this package. */
-#undef PACKAGE_URL
+#define PACKAGE_URL "https://github.com/sgminer-dev/sgminer"
 
 /* Define to the version of this package. */
-#undef PACKAGE_VERSION
+#define PACKAGE_VERSION "5.5.0-b"
 
 /* Define to l, ll, u, ul, ull, etc., as suitable for constants of type
    'ptrdiff_t'. */
-#undef PTRDIFF_T_SUFFIX
+/* #undef PTRDIFF_T_SUFFIX */
 
 /* Major version */
-#undef SGMINER_MAJOR_VERSION
+#define SGMINER_MAJOR_VERSION 5
 
 /* Micro version */
-#undef SGMINER_MINOR_SUBVERSION
+#define SGMINER_MINOR_SUBVERSION 0
 
 /* Minor version */
-#undef SGMINER_MINOR_VERSION
+#define SGMINER_MINOR_VERSION 5
 
 /* Path to sgminer install */
-#undef SGMINER_PREFIX
+#define SGMINER_PREFIX "/usr/local/bin"
 
 /* Revison */
-#undef SGMINER_REVISON
+#define SGMINER_REVISON b
 
 /* Define to l, ll, u, ul, ull, etc., as suitable for constants of type
    'sig_atomic_t'. */
-#undef SIG_ATOMIC_T_SUFFIX
+/* #undef SIG_ATOMIC_T_SUFFIX */
 
 /* Define to l, ll, u, ul, ull, etc., as suitable for constants of type
    'size_t'. */
-#undef SIZE_T_SUFFIX
+/* #undef SIZE_T_SUFFIX */
 
 /* If using the C implementation of alloca, define if you know the
    direction of stack growth for your system; otherwise it will be
@@ -327,79 +327,79 @@
 	STACK_DIRECTION > 0 => grows toward higher addresses
 	STACK_DIRECTION < 0 => grows toward lower addresses
 	STACK_DIRECTION = 0 => direction of growth unknown */
-#undef STACK_DIRECTION
+/* #undef STACK_DIRECTION */
 
 /* Define to 1 if you have the ANSI C header files. */
-#undef STDC_HEADERS
+#define STDC_HEADERS 1
 
 /* Defined to 1 if baikal support is wanted */
-#undef USE_BAIKAL
+#define USE_BAIKAL 1
 
 /* Use GIT version in version string */
-#undef USE_GIT_VERSION
+/* #undef USE_GIT_VERSION */
 
 /* Defined to 1 if opencl support is wanted */
-#undef USE_GPU
+/* #undef USE_GPU */
 
 /* Defined to 1 if usbutils support required */
-#undef USE_USBUTILS
+#define USE_USBUTILS 1
 
 /* Version number of package */
-#undef VERSION
+#define VERSION "5.5.0-b"
 
 /* Define to l, ll, u, ul, ull, etc., as suitable for constants of type
    'wchar_t'. */
-#undef WCHAR_T_SUFFIX
+/* #undef WCHAR_T_SUFFIX */
 
 /* Define to l, ll, u, ul, ull, etc., as suitable for constants of type
    'wint_t'. */
-#undef WINT_T_SUFFIX
+/* #undef WINT_T_SUFFIX */
 
 /* Define to 1 if on MINIX. */
-#undef _MINIX
+/* #undef _MINIX */
 
 /* Define to 2 if the system does not provide POSIX.1 features except with
    this defined. */
-#undef _POSIX_1_SOURCE
+/* #undef _POSIX_1_SOURCE */
 
 /* Define to 1 if you need to in order for `stat' and other things to work. */
-#undef _POSIX_SOURCE
+/* #undef _POSIX_SOURCE */
 
 /* "WinNT version for XP+ support" */
-#undef _WIN32_WINNT
+/* #undef _WIN32_WINNT */
 
 /* Define to 500 only on HP-UX. */
-#undef _XOPEN_SOURCE
+/* #undef _XOPEN_SOURCE */
 
 /* Enable extensions on AIX 3, Interix.  */
 #ifndef _ALL_SOURCE
-# undef _ALL_SOURCE
+# define _ALL_SOURCE 1
 #endif
 /* Enable GNU extensions on systems that have them.  */
 #ifndef _GNU_SOURCE
-# undef _GNU_SOURCE
+# define _GNU_SOURCE 1
 #endif
 /* Enable threading extensions on Solaris.  */
 #ifndef _POSIX_PTHREAD_SEMANTICS
-# undef _POSIX_PTHREAD_SEMANTICS
+# define _POSIX_PTHREAD_SEMANTICS 1
 #endif
 /* Enable extensions on HP NonStop.  */
 #ifndef _TANDEM_SOURCE
-# undef _TANDEM_SOURCE
+# define _TANDEM_SOURCE 1
 #endif
 /* Enable general extensions on Solaris.  */
 #ifndef __EXTENSIONS__
-# undef __EXTENSIONS__
+# define __EXTENSIONS__ 1
 #endif
 
 
 /* Define to `int' if <sys/types.h> doesn't define. */
-#undef gid_t
+/* #undef gid_t */
 
 /* Define to `__inline__' or `__inline' if that's what the C compiler
    calls it, or to nothing if 'inline' is not supported under any name.  */
 #ifndef __cplusplus
-#undef inline
+/* #undef inline */
 #endif
 
 /* Work around a bug in Apple GCC 4.0.1 build 5465: In C99 mode, it supports
@@ -415,7 +415,7 @@
 /* Define to the equivalent of the C99 'restrict' keyword, or to
    nothing if this is not supported.  Do not define if restrict is
    supported directly.  */
-#undef restrict
+#define restrict __restrict
 /* Work around a bug in Sun C++: it does not support _Restrict or
    __restrict__, even though the corresponding Sun C compiler ends up with
    "#define restrict _Restrict" or "#define restrict __restrict__" in the
@@ -427,10 +427,10 @@
 #endif
 
 /* Define to `unsigned int' if <sys/types.h> does not define. */
-#undef size_t
+/* #undef size_t */
 
 /* Define to `int' if <sys/types.h> doesn't define. */
-#undef uid_t
+/* #undef uid_t */
 
 /* Define as a marker that can be attached to declarations that might not
     be used.  This helps to reduce warnings, such as from
